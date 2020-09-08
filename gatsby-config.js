51a24af5e96c60daa3cdec369d7952f36c9baeba
iblurdesigner <nevomen@gmail.com>
2020-09-08 15:34:45 -0500

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Blur Dev Portfolio`,
+    description: `This is my personal porftfolio, you can find some of my work. We can change the world!`,
+    author: `@iblurdesigner`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
