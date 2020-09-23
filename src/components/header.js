35cce69a5df27a837a651043596f7275019bb99e
iblurdesigner <nevomen@gmail.com>
2020-09-22 22:32:33 -0500

    Creado el Layout del nav

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,48 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React from "react" // para el cart usar esto: { useContext }
+import { MenuItem, StyledHeader } from "../styles/components"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+const Header = () => {
+  // const { cart } = useContext(CartContext)
+  return (
+    <StyledHeader>
+      <Link to="/">
+        <img
+          src="https://drive.google.com/uc?export=view&id=1x1_43_I5cNkqI9729VsGs6Qr7h_9o4EX"
+          alt="logoeMotivoDev"
+        />
+      </Link>
+      <nav>
+        <ul>
+          <MenuItem margin>
+            <Link to="/about">About</Link>
+          </MenuItem>
+          <MenuItem margin>
+            <Link to="/portfolio">Portfolio</Link>
+          </MenuItem>
+          <MenuItem margin>
+            <Link to="/productos">Productos</Link>
+          </MenuItem>
+          <MenuItem margin>
+            <Link to="/contacto">Contacto</Link>
+          </MenuItem>
+          {/* <MenuItem>
+            <Link to="/cart">
+              <span>
+                <img
+                  src="https://i.postimg.cc/L6wpMxLt/cart.png"
+                  alt="cartlogo"
+                />
+                {cart.length}
+              </span>
+            </Link>
+          </MenuItem> */}
+        </ul>
+      </nav>
+    </StyledHeader>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,11 @@ import React from "react"
 import { Link } from "gatsby"
 import { Jumbo } from "../components"
 
-import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
+  <>
     <SEO title="Home" />
     <Jumbo />
     <button>CONTACT ME</button>
@@ -18,7 +17,7 @@ const IndexPage = () => (
     <Link to="https://github.com/iblurdesigner">iblurdesigner</Link>
     <Link to="https://www.linkedin.com/in/blurdev/">blurdev</Link>
     <Link to="https://www.behance.net/iblurdesigner">iblurdesigner</Link>
-  </Layout>
+  </>
 )
 
 export default IndexPage
